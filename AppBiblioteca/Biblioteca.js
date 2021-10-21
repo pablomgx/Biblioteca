7e87ad4545df992a3f3a1d1129600a7924a13553
lauraTaillefe <92849633+lauraTaillefe@users.noreply.github.com>
2021-10-21 11:41:28 +0200

    clase biblioteca

---
+++ AppBiblioteca/Biblioteca.js
@@ -0,0 +1,25 @@
+/**
+ * clase Biblioteca
+ */
+
+class Biblioteca{
+
+    /**
+     * constructor, funcion que se utiliza para crear un objeto de tipo Biblioteca
+     * 
+     * @param {*} id 
+     * @param {*} nombre 
+     * @param {*} direccion 
+     * @param {*} codigoPostal 
+     * @param {*} pais 
+     */
+    constructor(id, nombre, direccion, codigoPostal, pais){
+        this.id = id
+        this.nombre = nombre 
+        this.direccion = direccion
+        this.codigoPostal = codigoPostal
+        this.pais = pais
+    }
+
+///////////////////////////////////////////////////////////////////////////////////
+} 
