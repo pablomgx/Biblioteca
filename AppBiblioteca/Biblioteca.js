3e2663b3e74133ea49c7da2bc76b25599cf922b0
lauraTaillefe <92849633+lauraTaillefe@users.noreply.github.com>
2021-11-04 14:42:40 +0100

    Update Biblioteca.js

--- AppBiblioteca/Biblioteca.js
+++ AppBiblioteca/Biblioteca.js
@@ -26,10 +26,86 @@
         this.pais = pais
     }
 
+    librosConPrestamoExpirado(arrayTransaccion,arrayLibros){
+
+        console.log('\nLIBROS DE LA BIBLIOTECA CON PRESTAMO VENCIDO\n')
+        arrayTransaccion.forEach(transaccion => {
+            if (transaccion.fechaLimitePrestamo.getTime() > Date.now()){ //prestamo vencido
+                console.log('Libros de la biblioteca con prestamo Vencido : ', arrayLibros[transaccion.idLibroPrestado-1].titulo) 
+            }  
+        });
+    }
+
  }
 
- ////////////////////////////////////////////////////////////////////////////
- // PRUEBAS
+    function prestarLibro(arrayTransacciones, idLibroPrestado, idUsuario, idBibliotecario,tipoPrestamo){
+        //se crea un objeto transacción que se  añade al arrayTransacciones
+        //el id de la transaccion se genera a partir de la longitud del array
+        let transaccion = new Transaccion(arrayTransacciones.length+1, idLibroPrestado, idUsuario, idBibliotecario, tipoPrestamo)
+        arrayTransacciones.push(transaccion)
+
+        // mostrar por pantalla los datos de la transaccion que se acaba de realizar
+        // de id  = idTransaccion = arrayTransacciones.length - 1
+       // console.log( 'longitud del array transacciones: ',arrayTransacciones.length)
+       console.log('\nPRESTAMO REALIZADO\n') 
+       let idTransaccion = arrayTransacciones.length-1
+        console.log('Id del préstamo : ', arrayTransacciones[idTransaccion].id)
+        console.log('Libro del préstamo : ', arrayLibros[arrayTransacciones[idTransaccion].idLibroPrestado-1].titulo)
+        console.log('Usuario del préstamo : ', arrayUsuarios[arrayTransacciones[idTransaccion].idUsuarioAdquerido-1].nombre)
+        console.log('Bibliotecario del préstamo : ', arrayBibliotecarios[arrayTransacciones[idTransaccion].idFirmaBibliotecario-1].nombre)
+        console.log('Fecha del préstamo : ', arrayTransacciones[idTransaccion].fechaTransaccion)
+        console.log('Tipo de préstamo : ', arrayTransacciones[idTransaccion].tipoPrestamo)
+        console.log('Fecha Limite de Préstamo : ', arrayTransacciones[idTransaccion].fechaLimitePrestamo)
+
+        //se actualiza la propiedad prestado del objeto libro a true
+        arrayLibros[arrayTransacciones[idTransaccion].idLibroPrestado-1].prestado = true
+    }
+
+    function listarLibros(arrayLibros){
+        console.log ("\nLISTA DE LIBROS\n")
+
+        console.log('Titulo    //  Autor    //  Editorial //F.Prim.Edicion//Prestado//A la venta','\n')
+        arrayLibros.forEach(element => {
+           
+            console.log(element.titulo,'    ',element.autor,'    ',element.editorial,'    ',element.fechaPrimeraEdicion
+                      ,'    ',  element.prestado,'    ',element.venta,'\n')
+
+                    });
+    }
+
+    function totalPrestamosActuales(arrayTransacciones){
+        let totalPrestamosEnVigor = 0
+        console.log('\nTOTAL DE PRESTAMOS ACTUALES\n')
+        arrayTransacciones.forEach(transaccion =>{
+            console.log('Id del préstamo :: ', transaccion.id)
+            console.log('Libro del préstamo :: ', arrayLibros[transaccion.idLibroPrestado-1].titulo)
+            console.log('Usuario del préstamo :: ', arrayUsuarios[transaccion.idUsuarioAdquerido-1].nombre)
+            console.log('Bibliotecario del préstamo :: ', arrayBibliotecarios[transaccion.idFirmaBibliotecario-1].nombre)
+            console.log('Fecha del préstamo :: ', transaccion.fechaTransaccion)
+            console.log('Tipo de préstamo :: ', transaccion.tipoPrestamo)
+            console.log('Fecha Limite de Préstamo :: ', transaccion.fechaLimitePrestamo,'\n')
+           // console.log ('fecha hoy ::' , Date.now(), ' ',transaccion.fechaLimitePrestamo.getTime())
+            if (transaccion.fechaLimitePrestamo.getTime() > Date.now()){
+                totalPrestamosEnVigor = totalPrestamosEnVigor + 1
+            }
+        });
+        console.log('Total de préstamos en vigor :: \n', totalPrestamosEnVigor)
+        return totalPrestamosEnVigor
+    }
+
+    function  enviarAviso(arrayTransacciones){
+        for(let i=0;i<arrayTransacciones.length;i++){
+            if(arrayTransacciones[i].fechaLimitePrestamo<Date()){
+                console.log('El usuario con id '+arrayTransacciones[i].idUsuarioAdquerido+' con el id del libro '+arrayTransacciones[i].idLibroPrestado+' se ha pasado de la fecha limite./nFecha actual: '+Date()+'Fecha límite: '+arrayTransacciones[i].fechaLimitePrestamo)
+            }
+        }
+    }
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+ //////////////   PRUEBAS    ////////////////////
+
   //crear un array de libros 
   let arrayLibros = []
   // le asignamos como valor de id el de la siguiente posisión del array, empezando por el 0
@@ -75,106 +151,31 @@ console.log(arrayLibros)
    
   /////////////////////////////////////////////////
 
-
-    function prestarLibro(arrayTransacciones, idLibroPrestado, idUsuario, idBibliotecario,tipoPrestamo){
-        //se crea un objeto transacción que se  añade al arrayTransacciones
-        //el id de la transaccion se genera a partir de la longitud del array
-        let transaccion = new Transaccion(arrayTransacciones.length+1, idLibroPrestado, idUsuario, idBibliotecario, tipoPrestamo)
-        arrayTransacciones.push(transaccion)
-
-        // mostrar por pantalla los datos de la transaccion que se acaba de realizar
-        // de id  = idTransaccion = arrayTransacciones.length - 1
-        console.log( 'longitud del array transacciones: ',arrayTransacciones.length)
-        let idTransaccion = arrayTransacciones.length-1
-        console.log('Id del préstamo : ', arrayTransacciones[idTransaccion].id)
-        console.log('Libro del préstamo : ', arrayLibros[arrayTransacciones[idTransaccion].idLibroPrestado-1].titulo)
-        console.log('Usuario del préstamo : ', arrayUsuarios[arrayTransacciones[idTransaccion].idUsuarioAdquerido-1].nombre)
-        console.log('Bibliotecario del préstamo : ', arrayBibliotecarios[arrayTransacciones[idTransaccion].idFirmaBibliotecario-1].nombre)
-        console.log('Fecha del préstamo : ', arrayTransacciones[idTransaccion].fechaTransaccion)
-        console.log('Tipo de préstamo : ', arrayTransacciones[idTransaccion].tipoPrestamo)
-        console.log('Fecha Limite de Préstamo : ', arrayTransacciones[idTransaccion].fechaLimitePrestamo)
-
-        //se actualiza la propiedad prestado del objeto libro a true
-        arrayLibros[arrayTransacciones[idTransaccion].idLibroPrestado-1].prestado = true
-    }
-
-    function listarLibros(arrayLibros){
-        console.log ("\nLISTA DE LIBROS\n")
-
-        console.log('Titulo    //  Autor    //  Editorial //F.Prim.Edicion//Prestado//A la venta','\n')
-        arrayLibros.forEach(element => {
-           
-            console.log(element.titulo,'    ',element.autor,'    ',element.editorial,'    ',element.fechaPrimeraEdicion
-                      ,'    ',  element.prestado,'    ',element.venta,'\n')
-
-                    });
-    }
-
-    function totalPrestamosActuales(arrayTransacciones){
-        let totalPrestamosEnVigor = 0
-        arrayTransacciones.forEach(transaccion =>{
-            console.log("")
-            console.log('Id del préstamo :: ', transaccion.id)
-            console.log("")
-            console.log('Libro del préstamo :: ', arrayLibros[transaccion.idLibroPrestado-1].titulo)
-            console.log("")
-            console.log('Usuario del préstamo :: ', arrayUsuarios[transaccion.idUsuarioAdquerido-1].nombre)
-            console.log("")
-            console.log('Bibliotecario del préstamo :: ', arrayBibliotecarios[transaccion.idFirmaBibliotecario-1].nombre)
-            console.log("")
-            console.log('Fecha del préstamo :: ', transaccion.fechaTransaccion)
-            console.log("")
-            console.log('Tipo de préstamo :: ', transaccion.tipoPrestamo)
-            console.log("")
-            console.log('Fecha Limite de Préstamo :: ', transaccion.fechaLimitePrestamo)
-            console.log("")
-            console.log ('fecha hoy ::' , Date()+0)
-            console.log("")
-            if (transaccion.fechaLimitePrestamo < Date()+0){
-                totalPrestamosEnVigor = totalPrestamosEnVigor + 1
-            }
-        });
-        console.log('Total de préstamos en vigor :: ', totalPrestamosEnVigor)
-        return totalPrestamosEnVigor
-    }
-    function  enviarAviso(arrayTransacciones){
-        for(let i=0;i<arrayTransacciones.length;i++){
-            if(arrayTransacciones[i].fechaLimitePrestamo<Date()){
-                console.log('El usuario con id '+arrayTransacciones[i].idUsuarioAdquerido+' con el id del libro '+arrayTransacciones[i].idLibroPrestado+' se ha pasado de la fecha limite./nFecha actual: '+Date()+'Fecha límite: '+arrayTransacciones[i].fechaLimitePrestamo)
-            }
-        }
-    }
-
-////////////////////////////////////////////////////////
-
  // crear el array de transacciones a partir de la funcion prestarLibro()
     let arrayTransacciones = []
-    prestarLibro(arrayTransacciones,arrayLibros[1].id, arrayUsuarios[1].id,arrayBibliotecarios[1].id,-1)
+    prestarLibro(arrayTransacciones,arrayLibros[1].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,-1)
 
 ///////////////////////////////////////////////////////////////////////////////////
 
     // llamada a la funcion listarLibros con el arrayLibros
   
     //listarLibros(arrayLibros)
-    prestarLibro(arrayTransacciones,arrayLibros[2].id, arrayUsuarios[2].id,arrayBibliotecarios[1].id,-1)
-    prestarLibro(arrayTransacciones,arrayLibros[3].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,7)
-    prestarLibro(arrayTransacciones,arrayLibros[4].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,7)
-    prestarLibro(arrayTransacciones,arrayLibros[0].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,7)
+    prestarLibro(arrayTransacciones,arrayLibros[2].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,-1)
+    prestarLibro(arrayTransacciones,arrayLibros[3].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,1)
+    prestarLibro(arrayTransacciones,arrayLibros[4].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,1)
+    prestarLibro(arrayTransacciones,arrayLibros[0].id, arrayUsuarios[3].id,arrayBibliotecarios[1].id,1)
 
    //prestarLibro(arrayTransacciones,arrayLibros[3].id, arrayUsuarios[3].id,arrayBibliotecarios[3].id)
 
    //bibliotecario1.historialTransacciones(arrayTransacciones)
 
    totalPrestamosActuales(arrayTransacciones)
-   /*console.log("")
-   console.log("TRANSACCIONES: ")
-   console.log("")
-   prestarLibro(arrayTransacciones,arrayLibros[2].id, arrayUsuarios[3].id,arrayBibliotecarios[3].id,7)
-   bibliotecario1.historialTransacciones(arrayTransacciones)
-   bibliotecario2.historialTransacciones(arrayTransacciones)
-   bibliotecario3.historialTransacciones(arrayTransacciones)
-   bibliotecario4.historialTransacciones(arrayTransacciones)
-   bibliotecario5.historialTransacciones(arrayTransacciones)
-   bibliotecario1.enviarAviso(arrayTransacciones)*/
+
+//   usuario3.librosPrestados(arrayTransacciones,arrayLibros)
+///////////////////////////////////////
 
 
+//creación de la biblioteca
+
+    let biblioteca1 = new Biblioteca(1,'Gonzalez Garces','Coruña','14001','España')
+    biblioteca1.librosConPrestamoExpirado(arrayTransacciones,arrayLibros)
